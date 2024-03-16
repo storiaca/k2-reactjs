715ff64e71f1fa61167bde8f686d81e708d75bb0
Aleksandar Ristic <storiaca@gmail.com>
2024-03-16 11:48:01 +0100

    Add loader component

--- 005-useEffect/src/App.jsx
+++ 005-useEffect/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState, useEffect } from 'react';
 
 import { CardComponent } from './components/CardComponent';
+import LoadingComponent from './components/LoadingComponent';
 
 function App() {
   const [myData, setMyData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     fetch('https://dummyjson.com/products')
       .then((res) => res.json())
-      .then((data) => setMyData(data.products))
+      .then((data) => {
+        setMyData(data.products);
+        setIsLoading(true);
+      })
       .catch((err) => console.log(err));
   }, []);
 
@@ -16,11 +21,15 @@ function App() {
     <div className="container mx-auto">
       <h1 className="text-4xl">Hello ReactJS</h1>
 
-      <div className="flex flex-wrap gap-[20px] mt-28">
-        {myData.map((el) => {
-          return <CardComponent key={el.id} item={el} />;
-        })}
-      </div>
+      {isLoading ? (
+        <div className="flex flex-wrap gap-[20px] mt-28">
+          {myData.map((el) => {
+            return <CardComponent key={el.id} item={el} />;
+          })}
+        </div>
+      ) : (
+        <LoadingComponent />
+      )}
     </div>
   );
 }
---
+++ 005-useEffect/src/components/LoadingComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LoadingComponent() {
+  return <div className="text-center text-4xl uppercase">Loading Soon...</div>;
+}
+
+export default LoadingComponent;
