e0a1e34351318cd92153e2f41263e40c725b877e
Aleksandar Ristic <storiaca@gmail.com>
2024-03-16 12:38:59 +0100

    Install react icons

--- 005-useEffect/src/components/SingleUser.jsx
+++ 005-useEffect/src/components/SingleUser.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+// Icons
+import { useState } from 'react';
+import { MdChangeCircle } from 'react-icons/md';
 
 function SingleUser({ person, setPerson }) {
+  const [activeIcon, setActiveIcon] = useState(false);
+
   function handleUserChange() {
     setPerson(
       (prevState) =>
@@ -10,6 +14,8 @@ function SingleUser({ person, setPerson }) {
         }),
       // { name: 'Janko', age: 26, address: 'Jagodina' },
     );
+
+    setActiveIcon(true);
   }
   return (
     <div className="w-[300px] h-[300px] border-2 border-red-400 bg-blue-700 rounded-2xl flex flex-col items-center justify-evenly mx-auto mt-14">
@@ -18,13 +24,17 @@ function SingleUser({ person, setPerson }) {
       <h3 className="text-center mb-2 text-white uppercase">
         {person.address}
       </h3>
-
       <button
         onClick={handleUserChange}
-        className="p-2 border-2 border-solid border-white rounded-lg text-white uppercase"
+        className="flex gap-2 items-center p-2 border-2 border-solid border-white rounded-lg text-white uppercase"
       >
         Change Person
       </button>
+      {activeIcon ? (
+        <MdChangeCircle size={32} color={'red'} />
+      ) : (
+        <MdChangeCircle size={32} color={'white'} />
+      )}
     </div>
   );
 }
