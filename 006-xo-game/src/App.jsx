3410a82b9206e0e0ef4ad6bd13dc428d19d345b4
Aleksandar Ristic <storiaca@gmail.com>
2024-03-19 14:40:08 +0100

    06 OX - Game finished

--- 006-xo-game/src/App.jsx
+++ 006-xo-game/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import Cell from './components/Cell';
+
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cells, setCells] = useState(['', '', '', '', '', '', '', '', '']);
+
+  // ko igra prvi
+  const [firstGo, setFirstGo] = useState('circle');
+  // pobednik
+  const [winner, setWinner] = useState(null);
+
+  let checkArray = cells.every((cell) => cell !== '');
+
+  useEffect(() => {
+    checkWinner();
+  }, [cells]);
+
+  function checkWinner() {
+    let winnerCombination = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    winnerCombination.forEach((comb) => {
+      let crossWinner = comb.every((cell) => cells[cell] === 'cross');
+      let circleWinner = comb.every((cell) => cells[cell] === 'circle');
+
+      if (crossWinner) {
+        setWinner('Winner is Cross!!');
+        return;
+      } else if (circleWinner) {
+        setWinner('Winner is Circle!!');
+        return;
+      } else if (checkArray) {
+        setWinner('We dont have a winner');
+      }
+    });
+  }
+
+  function handleResetGame() {
+    if (winner || checkArray) {
+      let emptyArray = new Array(9).fill('');
+      setCells(emptyArray);
+      setWinner(null);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app">
+      <h1 className="title">X/O Game</h1>
+
+      <div className="squareContainer">
+        {cells.map((cell, index) => (
+          <Cell
+            key={index}
+            id={index}
+            cell={cell}
+            cells={cells}
+            firstGo={firstGo}
+            setFirstGo={setFirstGo}
+            setCells={setCells}
+          />
+        ))}
+        <button onClick={handleResetGame}>Reset Game</button>
+        {winner && <h2>{winner}</h2>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
