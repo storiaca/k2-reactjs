322d2e5180b2cb589144d826432946663e64d4a2
Aleksandar Ristic <storiaca@gmail.com>
2024-03-24 17:04:07 +0100

    Sharing state across components

--- 008-todo-app/todo-app/src/App.jsx
+++ 008-todo-app/todo-app/src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ToggleMode from "./components/ToggleMode";
 import TitleComponent from "./components/TitleComponent";
 import TodoInput from "./components/TodoInput";
 import TodoOutput from "./components/TodoOutput";
-import { useEffect } from "react";
+
 function App() {
   const [darkMode, setDarkMode] = useState(false);
   const [allTodo, setAllTodo] = useState([]);
 
-  useEffect(() => {
-    console.log(allTodo);
-  }, [allTodo]);
+  //useEffect(() => {}, [allTodo]);
+
   return (
-    <div
-      className={`${darkMode ? "dark" : "light"} flex flex-col justify-center`}
-    >
+    <div className={`${darkMode ? "dark" : "light"}`}>
       <ToggleMode darkMode={darkMode} setDarkMode={setDarkMode} />
       <TitleComponent>
         <h1 className="text-3xl font-bold text-center dark:text-white dark:bg-midnight">
--- 008-todo-app/todo-app/src/components/TodoOutput.jsx
+++ 008-todo-app/todo-app/src/components/TodoOutput.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import { useState } from "react";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { FaRegEdit } from "react-icons/fa";
+import Modal from "./Modal";
 
 function TodoOutput({ allTodo, setAllTodo }) {
+  const [open, setOpen] = useState(false);
+
   function handleRemove(id) {
     const removeItem = allTodo.filter((item) => item.id !== id);
     setAllTodo(removeItem);
   }
+
+  function editInputValue(e, id) {
+    setAllTodo([...allTodo, { id: id, title: e.target.value }]);
+  }
+
+  // function handleEdit(id) {
+  //   setOpen((prevState) => !prevState);
+  //   console.log(open);
+  //   const editItem = allTodo.filter((item) => item.id === id);
+  //   const [{ title }] = editItem;
+
+  //   <Modal open={open} onClose={() => setOpen(false)}>
+  //     <form>
+  //       <label className="sr-only" htmlFor="inputValue">
+  //         InputValue
+  //       </label>
+  //       <input
+  //         type="text"
+  //         id="inputValue"
+  //         onChange={(e) => editInputValue(e, id)}
+  //         value={title}
+  //       />
+  //     </form>
+  //   </Modal>;
+  // }
 
   // allTodo.push('Pera') // Number
 
@@ -14,19 +44,33 @@ function TodoOutput({ allTodo, setAllTodo }) {
         allTodo.map((todo) => (
           <div
             key={todo.id}
-            className="border min-w-80 mx-auto flex justify-between items-center gap-3"
+            className="border min-w-[364px] mx-auto flex justify-between items-center gap-3"
           >
             <p className="p-2 dark:text-white dark:bg-midnight">{todo.title}</p>
-            <button
-              className="ml-3 py-2 px-4 text-black dark:text-white dark:bg-midnight"
-              onClick={() => handleRemove(todo.id)}
-            >
-              Remove
-            </button>
+
+            <div className="btn-wrap">
+              <button
+                className="ml-3 py-2 text-black dark:text-white dark:bg-midnight"
+                onClick={() => setOpen(true)}
+              >
+                <FaRegEdit />
+              </button>
+              <button
+                className="ml-3 py-2 pr-2 text-black dark:text-white dark:bg-midnight"
+                onClick={() => handleRemove(todo.id)}
+              >
+                <FaRegTrashCan />
+              </button>
+            </div>
           </div>
         ))
       ) : (
         <p>Nema itema</p>
+      )}
+      {open && (
+        <Modal open={open} onClose={() => setOpen(false)}>
+          <h2>Modal radi</h2>
+        </Modal>
       )}
     </div>
   );
