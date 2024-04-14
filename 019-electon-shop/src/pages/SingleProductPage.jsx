82503134fcace6df812c57950ad8a773cf9443a7
Aleksandar Ristic <storiaca@gmail.com>
2024-04-14 13:34:08 +0200

    Design single produst page start

--- 019-electon-shop/src/pages/SingleProductPage.jsx
+++ 019-electon-shop/src/pages/SingleProductPage.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ProductsService from '../services/productsService';
+import Loader from '../components/Loader';
 
 function SingleProductPage() {
-  return <div>SingleProductPage</div>;
+  const [product, setProduct] = useState({});
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  // 1. uzmi ID
+  const { productId } = useParams();
+
+  useEffect(() => {
+    // 2. Poslati request.. Service
+    ProductsService.getSingleProduct(productId)
+      .then((res) => {
+        setProduct(res.data);
+        setIsLoading(true);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {isLoading ? (
+        <div>
+          {/* Left side */}
+          <div className="">
+            <img src={product.images[currentImage]} alt={product.title} />
+            <div className="flex items-center gap-5">
+              {product.images.map((imgSrc, index) => {
+                return (
+                  <img
+                    className="w-[100px] h-[100px] object-contain"
+                    src={imgSrc}
+                    key={index}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          {/* right side */}
+        </div>
+      ) : (
+        <div className="flex">
+          <Loader />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default SingleProductPage;
--- 019-electon-shop/src/services/productsService.js
+++ 019-electon-shop/src/services/productsService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 class ProductsService {
   static getAllCategories = () => axios.get('/products/categories');
   static getAllProducts = () => axios.get('/products');
+  static getSingleProduct = (id) => axios.get(`/products/${id}`);
 }
 
 export default ProductsService;
